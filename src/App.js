d873d08141a9a63a88a24b421ede96f9f8240b42
yeeun426 <thsudkcla7@naver.com>
2023-06-23 19:46:46 +0900

    feat : 라우팅

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Signup from './pages/Signup';
+import Login from './pages/Login';
+import Mypage from './pages/Mypage';
 
 function App() {
   return (
     <div className="App">
+      <Routes>
+        {/* <Route exact path='/*' element={<Main/>} /> */}
+        <Route path = '/login' element={<Login/>}></Route>
+        <Route path = '/signup' element={<Signup/>}></Route>
+        <Route path = '/mypage/*' element={<Mypage/>}></Route>
+      </Routes>
     </div>
   );
 }
