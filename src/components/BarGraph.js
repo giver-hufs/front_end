569154c193ee9dab5bfd58ebc46516fdfa00fa80
yeeun426 <thsudkcla7@naver.com>
2023-06-24 11:56:19 +0900

    feat :

--- src/components/BarGraph.js
+++ src/components/BarGraph.js
@@ -4,6 +4,8 @@ import styled from "styled-components";
 const GraphContainer = styled.div`
   display: flex;
   flex-direction: row;
+  align-items: center;
+  gap: 14px;
 `;
 
 const Bar = styled.div`
